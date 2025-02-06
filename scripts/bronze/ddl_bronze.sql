662b1bc2b5b632a00730cc4fef36005fb993b637
Steven Zeledon <128331125+stezele@users.noreply.github.com>
2025-02-06 16:44:43 -0600

    Add files via upload
    * add ddl for bronze and silver layer

---
+++ scripts/bronze/ddl_bronze.sql
@@ -0,0 +1,50 @@
+/*
+===============================================================================
+DDL Script: Create Bronze Tables
+===============================================================================
+Script Purpose:
+    This script creates tables in the 'bronze' schema, dropping existing tables 
+    if they already exist.
+	  Run this script to re-define the DDL structure of 'bronze' Tables
+===============================================================================
+*/
+
+
+GO
+CREATE SCHEMA bronze;
+GO
+
+IF OBJECT_ID ('bronze.hired_employees', 'U') IS NOT NULL
+	DROP TABLE bronze.hired_employees;
+GO
+
+CREATE TABLE bronze.hired_employees (
+	id				INT,
+	name			NVARCHAR(50),
+	datetime		NVARCHAR(50),
+	department_id   INT,
+	job_id          INT
+);
+GO
+
+
+IF OBJECT_ID ('bronze.departments', 'U') IS NOT NULL
+	DROP TABLE bronze.departments;
+
+GO
+CREATE TABLE bronze.departments (
+	id		    INT,
+	department	NVARCHAR(50)
+);
+GO
+
+
+IF OBJECT_ID ('bronze.jobs', 'U') IS NOT NULL
+	DROP TABLE bronze.jobs;
+
+GO
+CREATE TABLE bronze.jobs (
+	id		INT,
+	job 	NVARCHAR(50)
+);
+GO
---
+++ scripts/silver/ddl_silver.sql
@@ -0,0 +1,43 @@
+/*
+===============================================================================
+DDL Script: Create Silver Tables
+===============================================================================
+Script Purpose:
+    This script creates tables in the 'silver' schema, dropping existing tables 
+    if they already exist.
+	  Run this script to re-define the DDL structure of 'silver' Tables
+===============================================================================
+*/
+
+GO
+CREATE SCHEMA silver;
+GO
+
+IF OBJECT_ID ('silver.hired_employees', 'U') IS NOT NULL
+	DROP TABLE silver.hired_employees;
+CREATE TABLE silver.hired_employees (
+	id				INT,
+	name			NVARCHAR(50),
+	datetime		NVARCHAR(50),
+	department_id   INT,
+	job_id          INT
+);
+
+
+
+IF OBJECT_ID ('silver.departments', 'U') IS NOT NULL
+	DROP TABLE silver.departments;
+CREATE TABLE silver.departments (
+	id		    INT,
+	department	NVARCHAR(50)
+);
+
+
+
+IF OBJECT_ID ('silver.jobs', 'U') IS NOT NULL
+	DROP TABLE silver.jobs;
+CREATE TABLE silver.jobs (
+	id		INT,
+	job 	NVARCHAR(50)
+);
+
